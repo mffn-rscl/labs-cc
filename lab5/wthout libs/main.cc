81f78e73d3c7044b83292bbe53860966ec85f98c
anton <mffnrscl@gmail.com>
2023-12-21 04:05:27 +0200

    ✅

--- lab5/wthout libs/main.cc
+++ lab5/wthout libs/main.cc
@@ -2,34 +2,9 @@
 #include <iostream> // input/output
 #include <ctime> // for time()
 #include <cstdlib> // for rand() & srand()
-
-#include "methods.cc"
 #include "task.cc"
+#include "methods.hpp"
 
-void inputInOneLine(int size, int *array) {  for (int i = 0; i < size; i++) { std::cin >> array[i]; }}
-
-void randNumbersForArray(int size, int *array) // rewrite
-{
-    srand(static_cast<int>(time(nullptr))); //find an explain
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = rand() % 100; // generating an elements of array with range [-99; 99]
-        
-        if (array[i] % 2 == 0) {  array[i] = -array[i]; } // adding negative numbers
-       
-        std::cout << array[i]<< " ";
-    }
-
-}
-
-void inputSeparately(int size, int *array)
-{
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << "Enter the " << i + 1 << " value of array: ";
-        std::cin >> array[i];
-    }
-}
 
 void findTheMaxValFromArray(int size,int *array) // initializing func for two max numbers from array without using lib <climits>
 {
@@ -51,10 +26,21 @@ void findTheMaxValFromArray(int size,int *array) // initializing func for two ma
     
 }
 
+void inputInOneLine(int size, int *array) {  for (int i = 0; i < size; i++) { std::cin >> array[i]; }}
+
+void inputSeparately(int size, int *array)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter the " << i + 1 << " value of array: ";
+        std::cin >> array[i];
+    }
+}
+
 void ToSquaresWithTwoMax()
 {
     int size;
-    std::cout << "enter here length of array:";
+    std::cout << "Enter here length of array:";
     std::cin >> size;
     int array[size];
 
@@ -69,32 +55,36 @@ void ToSquaresWithTwoMax()
     {
          
         case 'r':
-            std::cout << "you chose to fill array by random. "<<std::endl;
-            std::cout << "finished array: ";
-
-            randNumbersForArray(size, array); 
+            std::cout << "You chose to fill array by random. "<<std::endl;
+            std::cout << "Finished array: ";
+            for (int i = 0; i < size; i++) 
+            { 
+                array[i] = randNums(); 
+                std::cout << array[i] << " ";
+            }
             break;
+       
         case 'y':
-            std::cout << "you chose to fill array by yourself. "<<std::endl;
-            std::cout << "fill the array in one line?(y/n): ";
+            std::cout << "You chose to fill array by yourself. "<<std::endl;
+            std::cout << "Fill the array in one line?(y/n): ";
             
-            char choiceLine;
-            std::cin >> choiceLine;
+            std::cin >> choice;
             
-            (choiceLine == 'y') ? inputInOneLine(size, array) : inputSeparately(size, array);
+            (choice == 'y') ? inputInOneLine(size, array) : inputSeparately(size, array);
            
-            std::cout << "finished array: ";      
+            std::cout << "Finished array: ";      
             
             for (int i = 0; i < size; i++) { std::cout << array[i] << " "; }
             
             break;
         default:
-            std::cerr << "incorrect input. Try again." << std::endl;
+            std::cerr << "\nIncorrect input. Try again." << std::endl;
+            exit(0);
             break;
     }
    
 
-    std::cout << "\nreplacing all positive numbers into their squares... " << std::endl;
+    std::cout << "\nReplacing all positive numbers into their squares.. " << std::endl;
 
     std::cout << "Result: ";
     for (int i = 0; i < size; i++)
@@ -110,6 +100,57 @@ void ToSquaresWithTwoMax()
 
 int main()
 {
+    initRandomSeed();
+    int choice;
+    std::cout << "Pick a task form the list below: \n" << std::endl;
+    std::cout << "To squares with two max values from list (1)." << std::endl; 
+    std::cout << "Table (2)." << std::endl;
+    std::cout << "Game: 'Guess a Number!' (3)." << std::endl;
+    std::cout << "Searching elements in list 1 (4)." << std::endl;
+    std::cout << "Plurals operations (5)." << std::endl;
+
+    do
+    {
+        std::cout << "\nPick a task number from list: ";
+        std::cin >> choice;
+        std::cin.ignore();
+
+        switch (choice)
+        {
+        case 0:
+            std::cout << "Ending process.." << std::endl;
+            break;
+
+        case 1:
+            std::cout << "You chose 'To squares with two max values from list'.\n" << std::endl;
+            ToSquaresWithTwoMax();
+            break;
+        case 2:
+            std::cout << "You chose task 'Table'.\n" << std::endl;
+            Table();
+            break;
+
+        case 3:
+            std::cout << "You chose game 'guess a number!'\n" << std::endl;
+            std::cout << "input here range from a to b: ";
+            long int a, b;
+            std::cin >> a >> b;
+            GuessedNumber(a, b);
+            break;
+        case 4: 
+            std::cout << "You chose 'Searching elements in list 1.'\n" << std::endl;
+
+            SearchingElementsInArray1();
+            break;
+        case 5: 
+            std::cout << "You chose 'Plurals operations'.\n" << std::endl;
+            plurals();
+            break;
+        default:
+            std::cerr << "Incorrect input.Try again." << std::endl;
+            break;
+        }
+    } while (choice != 0);
     
     return 0;
 }
