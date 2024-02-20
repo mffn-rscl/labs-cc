1232f18af58c2deed78b866376657bad3e125518
anton <mffnrscl@gmail.com>
2024-02-20 10:07:11 +0200

    d

--- lab6/program.cc
+++ lab6/program.cc
@@ -1,27 +1,44 @@
 #include <iostream>
 
-void list_sorting(int *arr, int size)
+void swap(int *a, int *b)
 {
-    
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int solution(int *arr, int size)
+void shell_sort(int data[], int size)
+{   
+    for (int interval = size / 2; interval > 0; interval /= 2) {
+        for (int i = interval; i < size; i += 1) {
+            int temp = data[i];
+            int j;
+            for (j = i; j >= interval && data[j - interval] > temp; j -= interval) {
+                data[j] = data[j - interval];
+            }
+            data[j] = temp;
+        }
+    }
+}
+
+int solution(int arr[], int size)
 {   
     int sum=0;
-    list_sorting(arr, size);
-    for (int count = 0; count < size - size/5; count++)sum+=arr[count];
+    shell_sort(arr, size);
+    for (int count = size/5; count < size; count++)sum+=arr[count];
     return sum;
 }
 
 int main()
 {
-    int size;
-    std::cout <<"Enter the size of shoppings:";
-    std::cin >> size;
-    int arr[size];
-    std::cout << "Enter the price of each stuffs:" << std::endl;
-    for (int i = 0; i < size; i++)std::cin >> arr[i];
-
-    std::cout << solution(arr, size) << std::endl;   
+        int size;
+        std::cout <<"Enter the size of shoppings:";
+        std::cin >> size;
+        int arr[size];
+        std::cout << "Enter the price of each stuffs:" << std::endl;
+        for (int i = 0; i < size; i++)std::cin >> arr[i];
+        std::cout << solution(arr, size) << std::endl;   
+   
+    
     return 0;
 }
