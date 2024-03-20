95ff4b3cc892a2a806e223de14a97a26ee3b7d2b
rascal <mffnrscl@gmail.com>
2024-03-20 11:51:41 +0200

    rtb

--- lab6/methods.hpp
+++ lab6/methods.hpp
@@ -4,7 +4,10 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
-
+void input(double *arr, int size)
+{
+    for (int i = 0; i < size; i++)std::cin >> arr[i];
+}
 void shell_sort(auto data[], int size) 
 {   
     for (int interval = size / 2; interval > 0; interval /= 2) {
